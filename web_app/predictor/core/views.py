0ee63a5fc4e9299738a75d8d2d7ce08b71de727d
utsav87589 <gkundukundu987@gmail.com>
2026-03-17 20:44:55 -0400

    working on showing the data

--- web_app/predictor/core/views.py
+++ web_app/predictor/core/views.py
@@ -1,6 +1,52 @@
 from django.shortcuts import render
+import requests
+import os
+from dotenv import load_dotenv
+
+### API key for the tiingo
+load_dotenv()
+api_key = os.getenv('key')
+print("API KEY:", api_key)
 
 # Create your views here.
 def home(request) : 
 
-    return render(request, 'core/home.html')
+    data = []  # ✅ instead of None
+    ticker = request.GET.get('ticker')
+
+    if ticker:
+        url = f"https://api.tiingo.com/tiingo/daily/{ticker}/prices"
+
+        headers = {
+            "Content-Type": "application/json"
+        }
+
+        params = {
+            "token": api_key
+        }
+
+        response = requests.get(url, headers=headers, params=params)
+
+        if response.status_code == 200:
+            json_data = response.json()
+
+            last_30 = json_data[-30:]
+
+            data = [
+                {
+                    "date": item["date"][:10],
+                    "open": item["open"],
+                    "high": item["high"],
+                    "low": item["low"],
+                    "close": item["close"],
+                }
+                for item in last_30
+            ]
+        else:
+            print("API failed:", response.status_code)
+
+        print("Ticker:", ticker)
+        print("Data length:", len(data))
+        print("Sample data:", data[:2])
+
+    return render(request, "core/home.html", {"data": data})
