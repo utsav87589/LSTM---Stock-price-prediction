fcf0a31a0c16738035691e6c75ac0a0d653bb144
utsav87589 <gkundukundu987@gmail.com>
2026-05-09 20:07:39 -0400

    change in the logic now to fetch the data and pass the multi features to plot the graphs

--- predictor_app/app/logic/data_fetcher.py
+++ predictor_app/app/logic/data_fetcher.py
@@ -1,13 +1,27 @@
 import yfinance as yf
 import plotly.express as px
 import json
+import plotly
+from datetime import date, timedelta
+
+
+today = date.today()
+end_date = today + timedelta(days = 1)
+start_date = today - timedelta(days = (365 * 6))
+
 
 def get_plot_data(ticker) : 
 
-    df = yf.download(ticker, start = '2020-05-08', end = '2026-05-09')
+    df = yf.download(ticker, start = start_date, end = end_date, multi_level_index = False)
+    df = df.drop('Volume', axis = 1)
 
-    fig = px.line(df, x = df.index, y = df[('Close', ticker).values])
+    all_graphs = {}
 
-    graph_JSON = json.dumps(fig, cls = plotly.utils.PlotlyJSONEncoder)
+    for feature in df.columns : 
 
-    return graph_JSON
+        fig = px.line(df, x = df.index, y = df[feature])
+        fig.update_layout(template="plotly_dark")
+
+        all_graphs[feature] = json.dumps(fig, cls = plotly.utils.PlotlyJSONEncoder)
+
+    return all_graphs
